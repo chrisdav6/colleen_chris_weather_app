ba9ea7bf176dff7808e80102d42dd4d0517238a7
Chris Davis <chris.davis5440@gmail.com>
2018-03-12 17:32:11 +0000

    Added first ajax call inside the other, using switch instaed of if/else

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,26 +1,24 @@
-
 $(function() {
-  
-  var forecast = "";
-  
+
   //Forecast AJAX Request
   $.ajax({
     url: "https://api.wunderground.com/api/3760db94f98044d8/forecast/q/NY/Troy.json",
     dataType: "jsonp"
   }).done(function(data) {
-    forecast = data.forecast.txt_forecast.forecastday[0].fcttext;
-  });
-  
-  //Current Temp AJAX Request
-  $.ajax({
-    url: "https://api.wunderground.com/api/3760db94f98044d8/conditions/q/NY/Troy.json",
-    dataType: "jsonp"
-  }).done(function(data) {
-    
-    console.log(data);
 
-    //Sayings
-    var sayings = {
+    console.log(data);
+    var forecast = data.forecast.txt_forecast.forecastday[0].fcttext;
+
+    //Current Temp AJAX Request
+    $.ajax({
+      url: "https://api.wunderground.com/api/3760db94f98044d8/conditions/q/NY/Troy.json",
+      dataType: "jsonp"
+    }).done(function(data) {
+
+      console.log(data);
+
+      //Sayings
+      var sayings = {
         clear: [
           "It's a beautiful day to be in Love with Colleen Davis!",
           "Christopher Loves Colleen Davis on this beautiful day more times than an infinite javascript loop!",
@@ -35,119 +33,100 @@ $(function() {
           "What a beautiful starry night to be in Love with Colleen Davis",
           "Stars in the sky and I Love Colleen to the moon and back * infinity!"
         ],
-        partlyCloudyDay : [
+        partlyCloudyDay: [
           "Freaking clouds! What the hell! I love you baby! <3"
         ],
-        partlyCloudyNight : [
+        partlyCloudyNight: [
           "Clouds in the night! What the hell! I love you baby! <3"
         ],
         cloudy: [
           "I Love Clouds! I Love Colleen!"
         ],
-        rain : [
+        rain: [
           "It's raining, it's pouring and I Love Colleen!",
-          "I really love raing days, snuggling with my baby!",
+          "I really love rainy days, snuggling with my baby!",
           "On this rainy day, I love my baby Colleen Davis!"
         ],
-        sleet : [
+        sleet: [
           "SLEET! What The! Ahh, who cares, because I Love Colleen Davis!",
         ],
-        snow : [
+        snow: [
           "It's snowing outside, and i want to be inside with my baby Colleen!",
           "The weather outside is frightful, but my baby Colleen is delightful!",
           "The amount of snowflakes falling still can not amount to how much I Love Colleen Davis!"
         ],
-        wind: [
-          "It's so windy out! Baby get over hear so i can snuggle you up!",
-        ],
         fog: [
           "The beuaty of my baby Colleen is so clear to me, even through the fog.",
         ]
-    };
-    
-    //Random Number for sayings
-    var randomClear = Math.floor(Math.random() * sayings.clear.length);
-    var randomClearNight = Math.floor(Math.random() * sayings.clearNight.length);
-    var randomPartlyCloudyDay = Math.floor(Math.random() * sayings.partlyCloudyDay.length);
-    var randomPartlyCloudyNight = Math.floor(Math.random() * sayings.partlyCloudyNight.length);
-    var randomCloudy = Math.floor(Math.random() * sayings.cloudy.length);
-    var randomRain = Math.floor(Math.random() * sayings.rain.length);
-    var randomSleet = Math.floor(Math.random() * sayings.sleet.length);
-    var randomSnow = Math.floor(Math.random() * sayings.snow.length);
-    var randomWind = Math.floor(Math.random() * sayings.wind.length);
-    var randomFog = Math.floor(Math.random() * sayings.fog.length);
-    
-    //Grab temp and summary, then add to frontend
-    var temp = Math.round(data.current_observation.temp_f);
-    var feelsTemp = data.current_observation.feelslike_f;
-    var summary = data.current_observation.weather;
-    
-    //For Testing
-    var summary= "Mostly Cloudy";
-    
-    $(".temp").html(temp + "&#176;");
-    $(".feels").html("Feels like " + feelsTemp + "&#176;");
-    $(".summary").html(summary);
-    $(".forecast").html(forecast);
-    
-   
-    
+      };
 
 
-    // if(summary === "clear") {
-    //   $("body").addClass("clear-day");
-    //   $(".message").text(sayings.clear[randomClear]);
-    // }
-  
-    
-    // if(summary === "clear-night") {
-    //   $("body").addClass("clear-night");
-    //   $(".message").text(sayings.clearNight[randomClearNight]);
-    // } 
-    
-    if(summary === "Mostly Cloudy") {
-      $("body").addClass("mostly-cloudy-day");
-      $("#icon").append("<img src='http://icons.wxug.com/i/c/v4/mostlycloudy.svg'>");
-      $(".message").text(sayings.partlyCloudyDay[randomPartlyCloudyDay]);
-    } 
-    
-    // if(summary === "partly-cloudy-night") {
-    //   $("body").addClass("partly-cloudy-night");
-    //   $(".message").text(sayings.partlyCloudyNight[randomPartlyCloudyNight]);
-    // } 
-    
-    // if(summary === "cloudy") {
-    //   $("body").addClass("cloudy");
-    //   $(".message").text(sayings.cloudy[randomCloudy]);
-    // } 
-    
-    // if(summary === "rain") {
-    //   $("body").addClass("rain");
-    //   $(".message").text(sayings.rain[randomRain]);
-    // } 
-    
-    // if(summary === "sleet") {
-    //   $("body").addClass("sleet");
-    //   $(".message").text(sayings.sleet[randomSleet]);
-    // } 
-    
-    // if(summary === "snow") {
-    //   $("body").addClass("snow");
-    //   $(".message").text(sayings.snow[randomSnow]);
-    // } 
-    
-    // if(summary === "wind") {
-    //   $("body").addClass("wind");
-    //   $(".message").text(sayings.wind[randomWind]);
-    // } 
-    
-    // if(summary === "fog") {
-    //   $("body").addClass("fog");
-    //   $(".message").text(sayings.fog[randomFog]);
-    // }
+      //Random Number for sayings
+      var randomClear = Math.floor(Math.random() * sayings.clear.length);
+      var randomClearNight = Math.floor(Math.random() * sayings.clearNight.length);
+      var randomPartlyCloudyDay = Math.floor(Math.random() * sayings.partlyCloudyDay.length);
+      var randomPartlyCloudyNight = Math.floor(Math.random() * sayings.partlyCloudyNight.length);
+      var randomCloudy = Math.floor(Math.random() * sayings.cloudy.length);
+      var randomRain = Math.floor(Math.random() * sayings.rain.length);
+      var randomSleet = Math.floor(Math.random() * sayings.sleet.length);
+      var randomSnow = Math.floor(Math.random() * sayings.snow.length);
+      var randomFog = Math.floor(Math.random() * sayings.fog.length);
 
-    
-   });
-  
-  
+      //Grab temp and summary, then add to frontend
+      var temp = Math.round(data.current_observation.temp_f);
+      var feelsTemp = data.current_observation.feelslike_f;
+      var summary = data.current_observation.weather;
+
+      //For Testing
+      // var summary = "Sleet";
+
+      $(".temp").html(temp + "&#176;");
+      $(".feels").html("Feels like " + feelsTemp + "&#176;");
+      $(".summary").html(summary);
+      $(".forecast").html(forecast);
+
+
+      switch (summary) {
+        case "Clear":
+        case "Mostly Sunny":
+        case "Partly Sunny":
+          $("body").addClass("clear-day");
+          $("#icon").append("<img src='http://icons.wxug.com/i/c/v4/clear.svg'>");
+          $(".message").text(sayings.clear[randomClear]);
+          break;
+        case "Mostly Cloudy":
+        case "Partly Cloudy":
+        case "Overcast":
+          $("body").addClass("mostly-cloudy-day");
+          $("#icon").append("<img src='http://icons.wxug.com/i/c/v4/mostlycloudy.svg'>");
+          $(".message").text(sayings.partlyCloudyDay[randomPartlyCloudyDay]);
+          break;
+        case "Cloudy":
+          $("body").addClass("cloudy");
+          $("#icon").append("<img src='http://icons.wxug.com/i/c/v4/cloudy.svg'>");
+          $(".message").text(sayings.cloudy[randomCloudy]);
+        case "Snow":
+        case "Flurries":
+          $("body").addClass("snow");
+          $("#icon").append("<img src='http://icons.wxug.com/i/c/v4/snow.svg'>");
+          $(".message").text(sayings.snow[randomSnow]);
+        case "Fog":
+        case "Hazy":
+          $("body").addClass("fog");
+          $("#icon").append("<img src='http://icons.wxug.com/i/c/v4/fog.svg'>");
+          $(".message").text(sayings.fog[randomFog]);
+        case "Rain":
+          $("body").addClass("rain");
+          $("#icon").append("<img src='http://icons.wxug.com/i/c/v4/rain.svg'>");
+          $(".message").text(sayings.rain[randomRain]);
+        case "Sleet":
+          $("body").addClass("sleet");
+          $("#icon").append("<img src='http://icons.wxug.com/i/c/v4/sleet.svg'>");
+          $(".message").text(sayings.sleet[randomSleet]);
+      }
+      
+
+    });
+  });
+
 });
