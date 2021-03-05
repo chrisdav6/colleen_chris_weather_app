cca5bc4e786834ba1029ec5b6c8683e0d2ed4bf4
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2021-03-05 09:10:19 -0500

    Troubleshooting Heroku

--- src/app.js
+++ src/app.js
@@ -1,23 +1,24 @@
-const path = require("path");
-const express = require("express");
-const darkSky = require("./utils/darkSky");
+const path = require('path');
+const express = require('express');
+const darkSky = require('./utils/darkSky');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
 //Use EJS
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 //Set Static Folders
-app.use(express.static(path.join(__dirname, "../public")));
-app.use(express.static(path.join(__dirname, "src/views")));
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, 'src/views')));
 
 //Route
-app.get("/", function (req, res) {
-  res.render("index");
+app.get('/', function(req, res) {
+  res.render('index');
 });
 
-app.get("/weather", function (rew, res) {
-  darkSky(42.738329, -73.654380, (error, weatherData) => {
+app.get('/weather', function(rew, res) {
+  darkSky(42.738329, -73.65438, (error, weatherData) => {
     if (error) {
       return res.send({
         error: error
@@ -25,12 +26,12 @@ app.get("/weather", function (rew, res) {
     }
 
     res.send({
-      forecast: weatherData,
+      forecast: weatherData
     });
   });
 });
 
 //Start Server
-app.listen(port, function () {
-  console.log("Server has started!");
+app.listen(port, function() {
+  console.log('Server has started!');
 });
